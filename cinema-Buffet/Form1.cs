ad1f4bbfe21cba50d32f4f91173ff0cd3f3d47b0
bedirhantosun <tosunb690@gmail.com>
2023-06-11 22:22:22 +0300

    Add project files.

---
+++ cinema-Buffet/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cinema_Buffet
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int cash = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int corn, ticket, water, tea, total;
+            corn = Convert.ToInt16(TxtCorn.Text);
+            ticket = Convert.ToInt16(TxtTicket.Text);
+            water = Convert.ToInt16(TxtWater.Text);
+            tea = Convert.ToInt16(TxtTea.Text);
+
+            total = corn * 4 + tea * 2 + water * 1 + ticket * 8;
+            LblTotal.Text = total.ToString()+ " TL ";
+
+            cash = cash + total;
+            LblCashBox.Text = cash.ToString() + " TL ";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            TxtTicket.Text = "";
+            TxtTea.Text = "";
+            TxtCorn.Text = "";
+            TxtWater.Text = "";
+            TxtCorn.Focus();
+        }
+
+        
+    }
+}
